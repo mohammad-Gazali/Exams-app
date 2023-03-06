fe6bd9d13c9c8da02c4197b230e8d85fd5346605
MohamadAlgazli <brawjym@gmail.com>
2023-03-06 23:00:54 +0300

    finishing firstly base.html without footer

--- main/models.py
+++ main/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+
+# class Option(models.Model):
+
+#     class Meta:
+#         verbose_name = _("Question")
+#         verbose_name_plural = _("Questions") 
+
+# class Question(models.Model):
+#     class Meta:
+#         verbose_name = _("Question")
+#         verbose_name_plural = _("Questions")
+
+
+# class EssayQuestion(models.Model):
+#     class Meta:
+#         verbose_name = _("Essay Question")
+#         verbose_name_plural = _("Essay Questions")
+
+
+# class Test(models.Model):
+#     class Meta:
+#         verbose_name = _("Test")
+#         verbose_name_plural = _("Tests")
--- static/js/base.js
+++ static/js/base.js
@@ -1,7 +1,10 @@
+const navbar = document.querySelector('nav');
 const sideNavbar = document.getElementById('side-navbar');
 const navbarToggleBtn = document.getElementById('mobile-toggle');
 const navbarToggleBtnClose = document.getElementById('mobile-toggle-close');
+const dropdownSidebar = document.querySelectorAll('.dropdown-side');
 const overlay = document.getElementById('overlay');
+
 
 
 navbarToggleBtn.onclick = () => {
@@ -17,4 +20,58 @@ overlay.onclick = () => {
 navbarToggleBtnClose.onclick = () => {
     overlay.classList.remove('active');
     sideNavbar.classList.remove('active');
+}
+
+// editing after reloading the page
+navbarBackground();
+
+window.onscroll = () => {
+    navbarBackground();
+}
+
+dropdownSidebar.forEach((dropdown) => {
+
+    const textDropdown = dropdown.querySelector('.dropdown-text-side');
+
+    textDropdown.onclick = () => {
+
+        if (dropdown.classList.contains("active")) {
+            
+            dropdownSidebar.forEach((d) => {
+                d.classList.remove("active");
+                d.querySelector('.dropdown-text-side').classList.remove("active");
+                d.querySelector('.dropdown-text-side i').style.transform = "scaleY(1)";
+            })
+
+        } else {
+
+            dropdownSidebar.forEach((d) => {
+                d.classList.remove("active");
+                d.querySelector('.dropdown-text-side').classList.remove("active");
+                d.querySelector('.dropdown-text-side').style.transform = "";
+            })
+
+            dropdown.classList.add("active");
+
+            textDropdown.classList.add("active");
+
+            textDropdown.querySelector("i").style.transform = "scaleY(-1)";
+
+        }
+        
+    }
+
+})
+
+
+
+
+//? Helper Functions
+
+function navbarBackground() {
+    if (window.scrollY === 0) {
+        navbar.style.setProperty('--nav-bg', 'transparent');
+    } else {
+        navbar.style.setProperty('--nav-bg', 'var(--overlay-clr)');
+    }
 }
